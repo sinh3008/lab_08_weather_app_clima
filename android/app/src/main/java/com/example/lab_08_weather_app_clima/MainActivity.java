c7a591f21c0275e9ae81cc57f5a85ace3d575a1c
sinhle3008 <sinhle.dev@gmail.com>
2023-07-29 10:45:35 +0700

    setting cơ bản

---
+++ android/app/src/main/java/com/example/lab_08_weather_app_clima/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.lab_08_weather_app_clima;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'screen/loading_screen.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: LoadingScreen(),
+    );
+  }
+}
+
+
