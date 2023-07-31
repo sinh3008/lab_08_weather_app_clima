04c8b40494985395111b2cb61bbcbbe15ac457b0
sinhle3008 <sinhle.dev@gmail.com>
2023-07-31 23:00:28 +0700

    project after create a file async

--- lib/screen/loading_screen.dart
+++ lib/screen/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,13 +9,29 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  // void getLocation() async {
+  //   Position position = await Geolocator.getCurrentPosition(
+  //       desiredAccuracy: LocationAccuracy.low);
+  //   print(position);
+  // }
+
+  void getLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           child: Text('Get Location'),
-          onPressed: (){},
+          onPressed: () {
+            getLocation();
+          },
         ),
       ),
     );
