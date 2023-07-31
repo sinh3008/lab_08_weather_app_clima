968b2ec8323dead0d10e481075ea17f3644c7d7f
sinhle3008 <sinhle.dev@gmail.com>
2023-07-31 22:42:41 +0700

    asyn future await full

--- scratch.dart
+++ scratch.dart
@@ -4,10 +4,10 @@ void main() {
   performTasks();
 }
 
-void performTasks() {
+void performTasks() async {
   task1();
-  task2();
-  task3();
+  String task2Result = await task2();
+  task3(task2Result);
 }
 
 void task1() {
@@ -15,18 +15,19 @@ void task1() {
   print('task 1 complete');
 }
 
-void task2() {
+Future task2() async {
   Duration threeSeconds = Duration(seconds: 3);
 
-  Future.delayed(threeSeconds, () {
-    String result = 'task 2 data';
+  String result = '';
+
+  await Future.delayed(threeSeconds, () {
+    result = 'task 2 data';
     print('task 2 complete');
   });
-
-
+  return result;
 }
 
-void task3() {
+void task3(String task2Result) {
   String result = 'task 3 data';
-  print('task 3 complete');
+  print('task 3 complete with $task2Result');
 }
